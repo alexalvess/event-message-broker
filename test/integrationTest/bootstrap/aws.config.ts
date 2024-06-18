8c6c5c724c927021d2137c2ed2f277e5117c62b9
alexalvess <alex.alves@vtex.com>
2024-06-18 17:06:44 +0200

    configure testContainers for rabbitmq

--- test/integrationTest/bootstrap/aws.config.ts
+++ test/integrationTest/bootstrap/aws.config.ts
@@ -12,12 +12,23 @@ beforeAll(async () => {
     }
 
     if(process.env.PROVIDER_TEST === 'aws') {
-        MessageBus.useAws();
         container = await new GenericContainer('localstack/localstack')
             .withExposedPorts(4566)
             .withEnvironment({ SERVICES: 'sqs,sns' })
             .withWaitStrategy(Wait.forLogMessage('Ready.'))
             .start();
+
+        MessageBus.useAws();
+    } else if(process.env.PROVIDER_TEST === 'rabbitmq') {        
+        container = await new GenericContainer('rabbitmq:3-management')
+            .withExposedPorts(5672, 15672)
+            .withEnvironment({ RABBITMQ_DEFAULT_USER: 'guest' })
+            .withEnvironment({ RABBITMQ_DEFAULT_PASS: 'guest' })
+            .withEnvironment({ RABBITMQ_PLUGINS: 'rabbitmq_management rabbitmq_delayed_message_exchange rabbitmq_defer' })
+            .withWaitStrategy(Wait.forLogMessage('Ready.'))
+            .start();
+
+        MessageBus.useRabbitMq();
     }
 });
 
